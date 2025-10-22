1022b4cc88abd56a5cf84ecc94d0b766da16521b
Pubkom BPMP Kepri <pubkombpmpkepri@gmail.com>
2025-10-22 12:39:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2194986673-2db2f",
+  "appId": "1:949860773902:web:5d0d630b7fe9206c188c5f",
+  "apiKey": "AIzaSyBtp07Y01Sg6LwOnMUpDW8TtKAKge6A6ZM",
+  "authDomain": "studio-2194986673-2db2f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "949860773902"
+};
