6e67a2abcb85d5a79d6d04cc2dbb03d5bddde0b6
Pubkom BPMP Kepri <pubkombpmpkepri@gmail.com>
2025-10-22 12:36:33 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyC1234567890abcdefghijklmnop",
+  authDomain: "sekola-digital.firebaseapp.com",
+  projectId: "sekola-digital",
+  storageBucket: "sekola-digital.appspot.com",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789012:web:1234567890abcdef123456"
 };
 
 // Initialize Firebase for client-side
