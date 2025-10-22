674687897decf34b9875a66424ee4c32171d66fe
Pubkom BPMP Kepri <pubkombpmpkepri@gmail.com>
2025-10-22 13:01:21 +0000

    Saya pakai email apkelana@gmail.com

--- src/config/admin.ts
+++ src/config/admin.ts
@@ -6,4 +6,5 @@ export const ALLOWED_ADMIN_EMAILS = [
   "admin1@bpmpkepri.go.id",
   "admin2@bpmpkepri.go.id",
   "test.admin@example.com", // Add your test admin email here
+  "apkelana@gmail.com",
 ];
