3aa563d338314abbeb9b7fc175fc85420087eef6
Pubkom BPMP Kepri <pubkombpmpkepri@gmail.com>
2025-10-22 15:45:35 +0000

    ERROR: failed to build: exit status 1 ERROR: failed to build: executing

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',                 // ⬅️ wajib untuk static export
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
   images: {
-    unoptimized: true,              // ⬅️ diperlukan untuk <Image> saat export
     remotePatterns: [
       { protocol: 'https', hostname: 'placehold.co' },
       { protocol: 'https', hostname: 'images.unsplash.com' },
