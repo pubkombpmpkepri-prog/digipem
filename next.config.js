909379aa8db3d24d7e5e206eb252cba637db692e
Pubkom BPMP Kepri <pubkombpmpkepri@gmail.com>
2025-10-23 06:20:09 +0000

    initial commit - upload Next.js project

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  images: { unoptimized: true },
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      { protocol: 'https', hostname: 'placehold.co' },
-      { protocol: 'https', hostname: 'images.unsplash.com' },
-      { protocol: 'https', hostname: 'picsum.photos' }
-    ]
-  }
 };
 module.exports = nextConfig;
