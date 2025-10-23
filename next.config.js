44845a240bc1ebd0e0f3ebbdff166c7b39271240
pubkombpmpkepri-prog <pubkombpmpkepri@gmail.com>
2025-10-23 07:59:32 +0000

    chore: switch to next standalone for App Hosting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'standalone',           // ⬅️ ganti dari 'export'
   images: { unoptimized: true },
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
